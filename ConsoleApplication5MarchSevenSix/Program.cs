b21a50c47b2510501160c7fb8782a54e6be1fbdf
Your Name <you.example.com>
2016-05-16 11:43:35 -0400

    submitting 5March

---
+++ ConsoleApplication5MarchSevenSix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5MarchSevenSix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Starting Number Here");
+            int startingNum  = int.Parse(Console.ReadLine());
+            // int n startingNum = 2;
+
+
+            Console.WriteLine("Enter Ending Number Here");
+            int endingNum = int.Parse(Console.ReadLine());
+           // int m endingNum = 6;
+      
+
+            decimal product = 1;
+            int n = startingNum;
+
+            do
+            {
+                if (n > endingNum)
+                {
+                    break;
+
+                }
+                product *= n;
+                n++;
+}
+            while (true);
+
+            Console.WriteLine("The product of this range is = " + product);
+        }
+    }
+}
